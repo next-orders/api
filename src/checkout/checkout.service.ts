81523289cf9a0cfe82b869beda29af4cdbe2728b
Nick Kosarev <hmbanan666@hotmail.com>
2023-10-07 15:34:25 +0200

    products now in db

--- src/checkout/checkout.service.ts
+++ src/checkout/checkout.service.ts
@@ -4,125 +4,36 @@ import { Checkout } from '../types';
 @Injectable()
 export class CheckoutService {
   /** Look up a checkout by id */
-  async findCheckoutById(id: string): Promise<Checkout> {
+  async findCheckoutById(id: string) {
     return {
       id,
       lines: [
         {
-          id: '1',
-          quantity: 1,
+          id: '123',
+          quantity: 2,
           variant: {
-            id: '12',
-            name: 'Европейская',
-            sku: '1212',
-            weight: {
-              unit: 'г',
-              value: 540,
-            },
-            pricing: {
-              onSale: false,
-              price: {
-                currency: 'RUB',
-                gross: {
-                  currency: 'RUB',
-                  amount: 689,
-                },
-                net: {
-                  currency: 'RUB',
-                  amount: 689,
-                },
-                tax: {
-                  currency: 'RUB',
-                  amount: 0,
-                },
-              },
-            },
+            id: 'phiny2u518hnjvxbuwe79tk0',
+            name: 'Четыре сезона',
+            createdAt: new Date(),
+            updatedAt: new Date(),
+            sku: '11',
+            weightUnit: 'г',
+            weightValue: 545,
+            onSale: false,
+            currency: 'RUB',
+            gross: 789,
+            net: 789,
+            tax: 0,
             media: [
               {
-                id: '12',
+                id: 'djbekzdi4srsdmwneuexn8fx',
                 alt: 'Фото',
-                url: '/static/products/pizza/12.jpeg',
-              },
-            ],
-          },
-        },
-        {
-          id: '2',
-          quantity: 1,
-          variant: {
-            id: '9',
-            name: 'Биг Чикен',
-            sku: '99',
-            weight: {
-              unit: 'г',
-              value: 560,
-            },
-            pricing: {
-              onSale: false,
-              price: {
-                currency: 'RUB',
-                gross: {
-                  currency: 'RUB',
-                  amount: 689,
-                },
-                net: {
-                  currency: 'RUB',
-                  amount: 689,
-                },
-                tax: {
-                  currency: 'RUB',
-                  amount: 0,
-                },
-              },
-            },
-            media: [
-              {
-                id: '9',
-                alt: 'Фото',
-                url: '/static/products/pizza/9.jpeg',
-              },
-            ],
-          },
-        },
-        {
-          id: '3',
-          quantity: 1,
-          variant: {
-            id: '5',
-            name: 'Суши Угорь',
-            sku: '55',
-            weight: {
-              unit: 'г',
-              value: 40,
-            },
-            pricing: {
-              onSale: false,
-              price: {
-                currency: 'RUB',
-                gross: {
-                  currency: 'RUB',
-                  amount: 140,
-                },
-                net: {
-                  currency: 'RUB',
-                  amount: 140,
-                },
-                tax: {
-                  currency: 'RUB',
-                  amount: 0,
-                },
-              },
-            },
-            media: [
-              {
-                id: '5',
-                alt: 'Фото',
-                url: '/static/products/sushi/5.jpeg',
+                url: '/static/products/pizza/1.png',
               },
             ],
           },
         },
       ],
-    };
+    } as Checkout;
   }
 }
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,23 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../db/prisma.service';
 import { Product } from '../types';
-import { categoryPizza } from './temp-data/pizza';
-import { categorySushi } from './temp-data/sushi';
-import { categoryWok } from './temp-data/wok';
-import { categoryBurger } from './temp-data/burger';
-import { categoryDessert } from './temp-data/dessert';
-import { categoryRoll } from './temp-data/roll';
 
 @Injectable()
 export class ProductService {
-  /** List of the shop's products */
-  async findProductsInCategory(id: string): Promise<Product[] | null> {
-    if (id === '1') return categoryPizza;
-    if (id === '2') return categorySushi;
-    if (id === '3') return categoryWok;
-    if (id === '4') return categoryBurger;
-    if (id === '5') return categoryDessert;
-    if (id === '6') return categoryRoll;
+  constructor(private readonly prisma: PrismaService) {}
 
-    return null;
+  /** List of the shop's products */
+  async findProductsInCategory(categoryId: string): Promise<Product[] | null> {
+    const products = await this.prisma.product.findMany({
+      where: { categoryId },
+      include: {
+        variants: {
+          include: {
+            media: true,
+          },
+        },
+      },
+    });
+    if (!products) {
+      return null;
+    }
+
+    return products;
   }
 }
