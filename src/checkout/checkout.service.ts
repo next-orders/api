882e0c2b57dfec1486a62a6725edb96b4a533dd1
Nick Kosarev <hmbanan666@hotmail.com>
2023-10-20 19:37:26 +0200

    fix

--- src/checkout/checkout.service.ts
+++ src/checkout/checkout.service.ts
@@ -7,44 +7,7 @@ import { createId } from '@paralleldrive/cuid2';
 
 let tempCheckout: Checkout = {
   id: '123',
-  lines: [
-    {
-      id: '123',
-      quantity: 2,
-      variant: {
-        id: 'phiny2u518hnjvxbuwe79tk0',
-        name: 'Four seasons',
-        slug: 'four-seasons',
-        description: '',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-        sku: '11',
-        weightUnit: 'g',
-        weightValue: 545,
-        onSale: false,
-        currency: 'USD',
-        gross: 7.89,
-        net: 7.89,
-        tax: 0,
-        seoTitle: null,
-        seoDescription: null,
-        category: {
-          id: 'hyw1yz8idynz72otujkbrnlp',
-          createdAt: new Date(),
-          updatedAt: new Date(),
-          name: 'Pizza',
-          slug: 'pizza',
-        },
-        media: [
-          {
-            id: 'f9jibjq7nqm5e3uwd343rer7',
-            alt: 'Four Seasons',
-            url: 'https://v1.next-orders.org/static/products/pizza/1.png',
-          },
-        ],
-      },
-    },
-  ],
+  lines: [],
 };
 
 @Injectable()
@@ -55,7 +18,7 @@ export class CheckoutService {
   ) {}
 
   async addProductToCheckout(id: string, dto: AddProductDto) {
-    Logger.log(`Checkout ${id}: adding product`);
+    Logger.log(`Checkout ${id}: adding product Id ${dto.productVariantId}`);
 
     // Find ProductVariant
     const variant = await this.productVariant.findProductVariantById(
@@ -65,22 +28,32 @@ export class CheckoutService {
       return null;
     }
 
-    // Max lines = 25
-    if (tempCheckout.lines.length >= 25) {
+    // Max lines = 20
+    if (tempCheckout.lines.length >= 20) {
       return null;
     }
 
-    // Product Found. Add one to Cart
-    const line: CheckoutLine = {
-      id: createId(),
-      quantity: 1,
-      variant,
-    };
+    // Check, if already in Cart
+    const findLine = tempCheckout.lines.find(
+      (line) => line.variant.id === variant.id,
+    );
+    const isAlreadyInCart = !!findLine;
+    if (isAlreadyInCart) {
+      // +1
+      findLine.quantity = findLine.quantity + 1;
+    } else {
+      // Add new to Cart
+      const newLine: CheckoutLine = {
+        id: createId(),
+        quantity: 1,
+        variant,
+      };
 
-    tempCheckout = {
-      ...tempCheckout,
-      lines: [...tempCheckout.lines, line],
-    };
+      tempCheckout = {
+        ...tempCheckout,
+        lines: [...tempCheckout.lines, newLine],
+      };
+    }
 
     return { ok: true, result: tempCheckout };
   }
